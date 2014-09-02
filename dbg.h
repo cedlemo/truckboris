16fa01344ec878a966ae9fac8c28fe9d7a57d929
cedlemo <cedlemo@gmx.com>
2014-09-02 18:42:15 +0200

    remove dbg.h

--- dbg.h
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-#include <cerrno>
-
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define SENTINEL(M, ...) printf("[SENTINEL] (%s:%s:%d: errno: %s) " M "\n",__FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-
